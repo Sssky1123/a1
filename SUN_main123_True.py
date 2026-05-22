c9aa29ca9ab472c60da5ba378e3f72dc66453305
HJMHJM11 <2278769756@qq.com>
2026-05-22 21:40:32 +0800

    upload all selected files

---
+++ SUN_main123_True.py
@@ -0,0 +1,237 @@
+import os,segyio,shutil,math,random,time
+from SUN_new123_True import *
+# from SNR import *
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
+
+mynet = myMDnet()
+
+model = mynet.get_MDnet()
+
+nxtr = 1
+nTrace = 750
+nPoint = 690
+nBlockT = 32
+nBlockX = 32
+epochs = 2
+novv = 1.5  # Window stride: larger value → less overlap, fewer windows
+# nGa = math.ceil(nGather / nxtr)
+input_num = 4
+output_num = 2
+nGather = 4
+if input_num == 1:
+    num= 1
+else:
+    num= nGather-(input_num-1)
+ff = 0  #Start reading from which gather (0 = start from the first gather)
+if input_num %2 == 1:
+    n = math.ceil((output_num-1)/2)
+    m = math.ceil((input_num-1)/2)
+    p = math.ceil((input_num+1)/2)
+else:
+    n = math.ceil((output_num) / 2)
+    m = math.ceil((input_num) / 2)
+
+sgypath1="mulcog97norm_cut2.sgy"
+sgypath2="oricog97norm_cut2.sgy"
+# sgypath3="Cprim_test_allgather.sgy"
+sgypath4="Ypredict_result_True.sgy"
+sgypath5="YCPT_mult_True.sgy"
+# sgypath6="Ysubtract.sgy"
+
+nOverlapT = math.floor(nBlockT / novv)
+nOverlapX = math.floor(nBlockX / novv)
+
+nBx = math.ceil((nPoint - nOverlapT) / (nBlockT - nOverlapT))
+if nBlockT == nPoint:
+    nBx = 1
+nBy = math.ceil((nTrace - nOverlapX) / (nBlockX - nOverlapX))
+if nBlockX == nTrace:
+    nBy = 1
+
+print("Reading Data")
+nnorig = np.zeros((nGather, 1))
+nnpred = np.zeros((nGather, 1))
+shutil.copyfile(sgypath1, sgypath4)
+shutil.copyfile(sgypath1, sgypath5)
+# shutil.copyfile(sgypath3, sgypath6)
+
+tr2=0
+for k in range(num):
+    tr1 = tr2
+    tr2 = tr1 + nxtr * nTrace
+    print('k=', k)
+    print('tr1=', tr1)
+    print('tr2=', tr2)
+    if tr2 > nGather * nTrace:
+        tr2 = nGather * nTrace
+        tr1 = (nGather - nxtr) * nTrace
+
+
+    # yibo = np.zeros((nPoint, nGather * nTrace))
+    # dy=list()
+    # if input_num % 2 == 1:
+    #     for i in range(-n,n+1):
+    #         with segyio.open(sgypath3, ignore_geometry=True) as f:
+    #             if input_num == 1:
+    #                 inp = f.trace.raw[(ff + k + i + n + m) * nTrace:(ff + k + i + n + p) * nTrace]
+    #             else:
+    #                 inp = f.trace.raw[(ff +k + i+m) * nTrace:(ff +k + i+p) * nTrace]
+    #             yibo = np.transpose(inp)
+    #             dy.append(yibo)
+    # else:
+    #     for i in range(output_num):
+    #         with segyio.open(sgypath3, ignore_geometry=True) as f:
+    #             inp = f.trace.raw[(ff + k + i + (m - n)) * nTrace:(ff + k + i + (m - n + 1)) * nTrace]
+    #             yibo = np.transpose(inp)
+    #             dy.append(yibo)
+
+    da = list()
+    nnorig= list()
+    dalabel= list()
+    if input_num % 2 == 1:
+        for i in range(-n,n+1):
+            with segyio.open(sgypath2, ignore_geometry=True) as f:
+                if input_num == 1:
+                    inp = f.trace.raw[(ff + k + i+n + m) * nTrace:(ff + k + i+n + p) * nTrace]
+                else:
+                    inp = f.trace.raw[(ff +k + i+m) * nTrace:(ff +k + i+p) * nTrace]
+                orig = np.transpose(inp)
+                nnorign = (abs(orig)).max()
+                nnorig.append(nnorign)
+                dalabeln = orig / nnorig[i+n]
+                dalabel.append(dalabeln)
+            temp = mynet.window_strate_vec(dalabel[i+n], nBlockT, nBlockX, nTrace, nPoint, nxtr, novv)
+            temp = np.squeeze(temp)
+            temp = np.reshape(temp, (-1, nBlockT, nBlockX, 1))
+            temp= np.transpose(temp, [0, 2, 1, 3])
+            da.append(temp)
+    else:
+        for i in range(output_num):
+            with segyio.open(sgypath2, ignore_geometry=True) as f:
+                inp = f.trace.raw[(ff + k + i + (m - n)) * nTrace:(ff + k + i + (m - n + 1)) * nTrace]
+                orig = np.transpose(inp)
+                nnorign = (abs(orig)).max()
+                nnorig.append(nnorign)
+                dalabeln = orig / nnorig[i]
+                dalabel.append(dalabeln)
+            temp = mynet.window_strate_vec(dalabel[i], nBlockT, nBlockX, nTrace, nPoint, nxtr, novv)
+            temp = np.squeeze(temp)
+            temp = np.reshape(temp, (-1, nBlockT, nBlockX, 1))
+            temp = np.transpose(temp, [0, 2, 1, 3])
+            da.append(temp)
+
+
+
+    if input_num == 1:
+        with segyio.open(sgypath1, ignore_geometry=True) as f:
+            inp = f.trace.raw[(ff + k+n) * nTrace:(ff + k+(n+1)) * nTrace]
+            prim1 = np.transpose(inp)
+            nnpred1 = (abs(prim1)).max()
+            dapred1 = prim1 / nnpred1
+
+            da11 = mynet.window_strate_vec(dapred1, nBlockT, nBlockX, nTrace, nPoint, nxtr, novv)
+            da11 = np.squeeze(da11)
+            da11 = np.reshape(da11, (-1, nBlockT, nBlockX, 1))
+            da11 = np.transpose(da11, [0, 2, 1, 3])
+            da31 = da11
+    else:
+        dm=list()
+        for i in range(input_num):
+            with segyio.open(sgypath1, ignore_geometry=True) as f:
+                inp = f.trace.raw[(ff + k + i ) * nTrace:(ff +k + i+1) * nTrace]
+                prim = np.transpose(inp)
+                nnpred = (abs(prim)).max()
+                dapred = prim / nnpred
+            temp = mynet.window_strate_vec(dapred, nBlockT, nBlockX, nTrace, nPoint, nxtr, novv)
+            temp = np.squeeze(temp)
+            temp = np.reshape(temp, (-1, nBlockT, nBlockX, 1))
+            temp = np.transpose(temp, [0, 2, 1, 3])
+            dm.append(temp)
+
+
+    print("Training")
+
+    t1 = time.perf_counter()
+    model.compile(optimizer=Adam(lr=3e-4), loss=myloss)  #First timing
+    epp = epochs
+    if input_num == 1:
+        history = model.fit(da11, da, batch_size=16, epochs=epp, validation_split=0, verbose=2, shuffle=True)
+    else:
+        history = model.fit(dm, da, batch_size=16, epochs=epp, validation_split=0, verbose=2, shuffle=True)
+    t2 = time.perf_counter()
+    delta_time = t2 - t1
+    print("Training time (seconds)：", delta_time)
+
+    t3 = time.perf_counter()
+    if input_num == 1:
+        fieldpredict_result = model.predict(da11, batch_size=1, verbose=1)
+    else:
+        fieldpredict_result = model.predict(dm, batch_size=1, verbose=1)
+    t4 = time.perf_counter()
+    delta_time = t4 - t3
+    print("Inference time (seconds)：", delta_time)
+
+
+    if input_num % 2 == 1:
+        for i in range(-n,n+1):
+            if output_num==1:
+                fieldpredict_resultn = np.squeeze(fieldpredict_result)
+            else:
+                fieldpredict_resultn = np.squeeze(fieldpredict_result[i+n])
+            fieldpredict_resultn = np.transpose(fieldpredict_resultn, [0, 2, 1])
+
+            arr3d = np.zeros((nBx * nBy * nxtr, nBlockT, nBlockX))
+            for ii in range(nBx * nBy * nxtr):
+                arr3d[ii, :, :] = fieldpredict_resultn[ii, :, :]
+            mubld = mynet.matblend(arr3d, nxtr, nTrace, nPoint, nBlockT, nBlockX, novv)
+
+            if input_num == 1:
+                with segyio.open(sgypath4, "r+", strict=False) as f:
+                    f.trace.raw[(ff + k + i + n + m) * nTrace:(ff + k + i + n + p) * nTrace] = np.transpose(mubld) * nnorig[i + n]
+
+                with segyio.open(sgypath5, "r+", strict=False) as f:
+                    f.trace.raw[(ff + k + i + n + m) * nTrace:(ff + k + i + n + p) * nTrace] = np.transpose(dalabel[i + n] - mubld) * nnorig[i + n]
+
+                # with segyio.open(sgypath6, "r+", strict=False) as f:
+                #     f.trace.raw[(ff + k + i + n + m) * nTrace:(ff + k + i + n + p) * nTrace] = np.transpose(dy[i + n] - (dalabel[i + n] - mubld) * nnorig[i + n])
+            else:
+                with segyio.open(sgypath4, "r+", strict=False) as f:
+                    f.trace.raw[(ff +k + i+m) * nTrace:(ff +k + i+p) * nTrace] = np.transpose(mubld) * nnorig[i+n]
+
+                with segyio.open(sgypath5, "r+", strict=False) as f:
+                    f.trace.raw[(ff +k + i+m) * nTrace:(ff +k + i+p) * nTrace] = np.transpose(dalabel[i+n] - mubld) * nnorig[i+n]
+
+                # with segyio.open(sgypath6, "r+", strict=False) as f:
+                #     f.trace.raw[(ff +k + i+m) * nTrace:(ff +k + i+p) * nTrace] = np.transpose(dy[i+n] - (dalabel[i+n] - mubld) * nnorig[i+n])
+
+    else:
+        for i in range(output_num):
+            fieldpredict_resultn = np.squeeze(fieldpredict_result[i])
+            fieldpredict_resultn = np.transpose(fieldpredict_resultn, [0, 2, 1])
+
+            arr3d = np.zeros((nBx * nBy * nxtr, nBlockT, nBlockX))
+            for ii in range(nBx * nBy * nxtr):
+                arr3d[ii, :, :] = fieldpredict_resultn[ii, :, :]
+            mubld = mynet.matblend(arr3d, nxtr, nTrace, nPoint, nBlockT, nBlockX, novv)
+            with segyio.open(sgypath4, "r+", strict=False) as f:
+                f.trace.raw[(ff + k + i + (m - n)) * nTrace:(ff + k + i + (m - n + 1)) * nTrace] = np.transpose(mubld) * nnorig[i]
+
+            with segyio.open(sgypath5, "r+", strict=False) as f:
+                f.trace.raw[(ff + k + i + (m - n)) * nTrace:(ff + k + i + (m - n + 1)) * nTrace] = np.transpose(dalabel[i] - mubld) * nnorig[i]
+
+            # with segyio.open(sgypath6, "r+", strict=False) as f:
+            #     f.trace.raw[(ff + k + i + (m - n)) * nTrace:(ff + k + i + (m - n + 1)) * nTrace] = np.transpose(dy[i] - (dalabel[i] - mubld) * nnorig[i])
+
+
+    if k == 0:
+        plt.plot(history.history['loss'], label='training loss', color='b')
+        plt.title('model loss')
+        plt.ylabel('loss')
+        plt.xlabel('epochs')
+        plt.legend()
+        plt.savefig('Trainloss.png')
+
---
+++ SUN_new123_True.py
@@ -0,0 +1,240 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+import math
+# import paddle
+import tensorflow as tf
+
+print('GPU',tf.test.is_gpu_available())
+
+import numpy as np
+from tensorflow.keras.models import *
+from tensorflow.keras.optimizers import *
+from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from tensorflow.keras.layers import *
+from tensorflow.contrib.keras import models, layers, regularizers
+from tensorflow.contrib.keras import backend as K
+import pandas as pd
+import matplotlib.pyplot as plt
+# from skimage.metrics import structural_similarity as compare_ssim
+# from scipy.io import loadmat
+from scipy import io
+
+nBlockT = 32
+nBlockX = 32
+input_num = 4
+output_num = 2
+
+
+
+def myloss(y_true, y_pred):
+
+    l1  = K.mean(K.abs(y_true - y_pred), axis=-1)
+    loss = l1
+
+    return loss
+
+class myMDnet(object):
+
+    def __init__(self, img_rows=None, img_cols=None):
+        self.img_rows = img_rows
+        self.img_cols = img_cols
+
+
+    def unet(self,m=[]):
+        if input_num == 1:
+            input_m = m
+        else:
+            input_m = concatenate(m, axis=3)
+        nm = 64
+        kn = 3
+        ag = 1e-5
+        conv1 = Conv2D(nm, kn, padding='same', kernel_initializer='glorot_normal',
+                       kernel_regularizer=regularizers.l2(ag))(input_m)
+        # conv1 = BatchNormalization()(conv1)
+        conv1 = Activation('relu')(conv1)
+        conv1 = Conv2D(nm, kn, padding='same', kernel_initializer='glorot_normal',
+                       kernel_regularizer=regularizers.l2(ag))(conv1)
+        # conv1 = BatchNormalization()(conv1)
+        conv1 = Activation('relu')(conv1)
+        pool1 = MaxPooling2D(pool_size=(2, 2), padding='same')(conv1)
+
+        conv2 = Conv2D(nm * 2, kn, padding='same', kernel_initializer='glorot_normal',
+                       kernel_regularizer=regularizers.l2(ag))(pool1)
+        # conv2 = BatchNormalization()(conv2)
+        conv2 = Activation('relu')(conv2)
+        conv2 = Conv2D(nm * 2, kn, padding='same', kernel_initializer='glorot_normal',
+                       kernel_regularizer=regularizers.l2(ag))(conv2)
+        # conv2 = BatchNormalization()(conv2)
+        conv2 = Activation('relu')(conv2)
+        pool2 = MaxPooling2D(pool_size=(2, 2), padding='same')(conv2)
+
+        conv3 = Conv2D(nm * 4, kn, padding='same', kernel_initializer='glorot_normal',
+                       kernel_regularizer=regularizers.l2(ag))(pool2)
+        # conv3 = BatchNormalization()(conv3)
+        conv3 = Activation('relu')(conv3)
+        conv3 = Conv2D(nm * 4, kn, padding='same', kernel_initializer='glorot_normal',
+                       kernel_regularizer=regularizers.l2(ag))(conv3)
+        # conv3 = BatchNormalization()(conv3)
+        conv3 = Activation('relu')(conv3)
+        pool3 = MaxPooling2D(pool_size=(2, 2), padding='same')(conv3)
+
+        conv4 = Conv2D(nm * 8, kn, padding='same', kernel_initializer='glorot_normal',
+                       kernel_regularizer=regularizers.l2(ag))(pool3)
+        # conv4 = BatchNormalization()(conv4)
+        conv4 = Activation('relu')(conv4)
+        conv4 = Conv2D(nm * 8, kn, padding='same', kernel_initializer='glorot_normal',
+                       kernel_regularizer=regularizers.l2(ag))(conv4)
+        # conv4 = BatchNormalization()(conv4)
+        conv4 = Activation('relu')(conv4)
+        up5 = Conv2DTranspose(nm * 4, (2, 2), strides=(2, 2), activation='relu', padding='same',
+                              kernel_initializer='glorot_normal', kernel_regularizer=regularizers.l2(ag))(conv4)
+        print(up5.shape)
+        merge5 = concatenate([conv3, up5], axis=3)
+        print(merge5.shape)
+
+        conv5 = Conv2D(nm * 4, kn, padding='same', kernel_initializer='glorot_normal',
+                       kernel_regularizer=regularizers.l2(ag))(merge5)
+        # conv5 = BatchNormalization()(conv5)
+        conv5 = Activation('relu')(conv5)
+        conv5 = Conv2D(nm * 4, kn, padding='same', kernel_initializer='glorot_normal',
+                       kernel_regularizer=regularizers.l2(ag))(conv5)
+        # conv5 = BatchNormalization()(conv5)
+        conv5 = Activation('relu')(conv5)
+        up6 = Conv2DTranspose(nm * 2, (2, 2), strides=(2, 2), activation='relu', padding='same',
+                              kernel_initializer='glorot_normal', kernel_regularizer=regularizers.l2(ag))(conv5)
+        merge6 = concatenate([conv2, up6], axis=3)
+
+        conv6 = Conv2D(nm * 2, kn, padding='same', kernel_initializer='glorot_normal',
+                       kernel_regularizer=regularizers.l2(ag))(merge6)
+        # conv6 = BatchNormalization()(conv6)
+        conv6 = Activation('relu')(conv6)
+        conv6 = Conv2D(nm * 2, kn, padding='same', kernel_initializer='glorot_normal',
+                       kernel_regularizer=regularizers.l2(ag))(conv6)
+        # conv6 = BatchNormalization()(conv6)
+        conv6 = Activation('relu')(conv6)
+        up7 = Conv2DTranspose(nm, (2, 2), strides=(2, 2), activation='relu', padding='same',
+                              kernel_initializer='glorot_normal', kernel_regularizer=regularizers.l2(ag))(conv6)
+        merge7 = concatenate([conv1, up7], axis=3)
+
+        conv7 = Conv2D(nm, kn, activation='relu', padding='same', kernel_initializer='glorot_normal',
+                       kernel_regularizer=regularizers.l2(ag))(merge7)
+        conv7 = Conv2D(nm, kn, activation='relu', padding='same', kernel_initializer='glorot_normal',
+                       kernel_regularizer=regularizers.l2(ag))(conv7)
+        conv7 = Conv2D(output_num, 1, activation='tanh')(conv7)
+
+        return conv7
+
+
+
+    def get_MDnet(self):
+        if input_num == 1:
+            m = Input((nBlockT, nBlockX, 1))
+            y = self.unet(m)
+        else:
+            m=list()
+            for i in range(input_num):
+                m1 = Input((nBlockT, nBlockX, 1))
+                m.append(m1)
+            y = self.unet(m)
+
+
+        out = list()
+        for i in range(output_num):
+            output1 = Reshape((nBlockT, nBlockX, 1))(y[:, :, :, i])
+            out.append(output1)
+        model = Model(inputs=m, outputs=out)
+
+
+        return model
+
+
+
+    def matblend(self, orgg, nGather, nTrace, nPoint, nBlockT, nBlockX, novv):
+        nOverlapT = math.floor(nBlockT / novv)
+        nOverlapX = math.floor(nBlockX / novv)
+
+        weightX = np.hanning(nBlockT + 2)[1:-1]
+        weightY = np.hanning(nBlockX + 2)[1:-1]
+        Weight2D = np.kron(weightX, np.transpose(weightY))
+        Weight2D = np.reshape(Weight2D, (nBlockT, nBlockX))
+
+        nBx = math.ceil((nPoint - nOverlapT) / (nBlockT - nOverlapT))
+        if nBlockT == nPoint:
+            nBx = 1
+        nBy = math.ceil((nTrace - nOverlapX) / (nBlockX - nOverlapX))
+        if nBlockX == nTrace:
+            nBy = 1
+
+        ePriL2 = np.zeros((nPoint, nGather * nTrace))
+        ePriWeightL2 = np.zeros((nPoint, nGather * nTrace))
+
+        for ii in range(nGather):
+
+            for iBx in range(nBx):
+                xStart = iBx * (nBlockT - nOverlapT) + 1
+                xEnd = xStart + nBlockT - 1
+                if xEnd > nPoint:
+                    xEnd = nPoint
+                    xStart = xEnd - nBlockT + 1
+                for iBy in range(nBy):
+                    yStart = iBy * (nBlockX - nOverlapX) + 1
+                    yEnd = yStart + nBlockX - 1
+                    if yEnd > nTrace:
+                        yEnd = nTrace
+                        yStart = yEnd - nBlockX + 1
+
+                    orgBlock = orgg[ii * nBx * nBy + iBx * nBy + iBy, :, :]
+                    orgvv = np.reshape(orgBlock, (nBlockT, nBlockX), 'F')
+
+                    ePriL2[xStart - 1:xEnd, ii * nTrace + yStart - 1:ii * nTrace + yEnd] = ePriL2[xStart - 1:xEnd,
+                                                                                           ii * nTrace + yStart - 1:ii * nTrace + yEnd] + orgvv * Weight2D
+                    ePriWeightL2[xStart - 1:xEnd, ii * nTrace + yStart - 1:ii * nTrace + yEnd] = ePriWeightL2[
+                                                                                                 xStart - 1:xEnd,
+                                                                                                 ii * nTrace + yStart - 1: ii * nTrace + yEnd] + Weight2D
+
+        for ii in range(nPoint):
+            for jj in range(nGather * nTrace):
+                if ePriWeightL2[ii, jj] == 0:
+                    ePriWeightL2[ii, jj] = 1
+                ePriL2[ii, jj] = ePriL2[ii, jj] / ePriWeightL2[ii, jj]
+
+        return ePriL2
+
+    def window_strate_vec(self, orgg, nBlockT, nBlockX, nTrace, nPoint, nGather, novv):
+        nOverlapT = math.floor(nBlockT / novv)
+        nOverlapX = math.floor(nBlockX / novv)
+        nBx = math.ceil((nPoint - nOverlapT) / (nBlockT - nOverlapT))
+        if nBlockT == nPoint:
+            nBx = 1
+        nBy = math.ceil((nTrace - nOverlapX) / (nBlockX - nOverlapX))
+        if nBlockX == nTrace:
+            nBy = 1
+
+        orgBlock = np.zeros((nBlockT, nBlockX))
+
+        GG = np.zeros((nGather * nBx * nBy, nBlockT, nBlockX, 1))
+
+        for ii in range(nGather):
+
+            for iBx in range(nBx):
+                xStart = iBx * (nBlockT - nOverlapT) + 1
+                xEnd = xStart + nBlockT - 1
+                if xEnd > nPoint:
+                    xEnd = nPoint
+                    xStart = xEnd - nBlockT + 1
+                for iBy in range(nBy):
+                    yStart = (iBy) * (nBlockX - nOverlapX) + 1
+                    yEnd = yStart + nBlockX - 1
+                    if yEnd > nTrace:
+                        yEnd = nTrace
+                        yStart = yEnd - nBlockX + 1
+
+                    orgBlock[:, :] = orgg[xStart - 1:xEnd, ii * nTrace + yStart - 1:ii * nTrace + yEnd]
+
+                    GG[ii * nBx * nBy + iBx * nBy + iBy, :, :, 0] = orgBlock
+
+        return GG
+
+if __name__ == '__main__':
+    mynet = myMDnet()  # # Instantiate class
+    mynet.train()  # Method in startup class
